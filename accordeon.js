5ae17d9461bdc694a5172f5aa225e579f3213f0e
Sylvester Engelen <sylvester@lemone.com>
2019-12-23 14:02:48 +0100

    Added hover effect with caption

--- accordeon.js
+++ accordeon.js
@@ -5,12 +5,15 @@ let accordion = class Accordion {
         this.spawned = false;
         this.tickrate = 1000;
         this.isActive = true;
+        this.caption = [];
     }
 
-    addSlide(image) {
+    addSlide(image, caption) {
         if (this.slides <= 0) { this.slides = 0 }
         this.slides += 1;
         this.image.push(image);
+        this.caption.push(caption);
+        console.log(caption)
         this.init()
     }
 
@@ -20,19 +23,19 @@ let accordion = class Accordion {
         this.init();
     }
 
-    init() {
-        document.getElementById('accordion').innerHTML = `<ul id="inneracc"></ul>`;
-        for (let i = 0; i < this.slides; i++) {
-            document.getElementById('inneracc').innerHTML += `<li onclick="acc.currentSlide(${i})" id="item" class="${i} acc-elm"><img src=${this.image[i]} class="image"></li>`;
-            document.getElementById('inneracc').firstElementChild.className = '0 acc-elm current';
-        }
-    }
-
     currentSlide(id) {
         for (let i = 0; i < this.slides; i++) {
             console.log(document.getElementsByClassName(`${i}`)[0].classList.remove('current'))
         }
         document.getElementsByClassName(`${id}`)[0].classList.add('current');
+    }
+
+    init() {
+        document.getElementById('accordion').innerHTML = `<ul id="inneracc"></ul>`;
+        for (let i = 0; i < this.slides; i++) {
+            document.getElementById('inneracc').innerHTML += `<li onclick="acc.currentSlide(${i})" onmouseover="acc.currentSlide(${i})" id="item" class="${i} acc-elm"><img src=${this.image[i]} class="image ${i}"><pre class="stateless"><span>${this.caption[i]}</span></pre></li>`;
+            document.getElementById('inneracc').firstElementChild.className = '0 acc-elm current';
+        }
     }
 
     autoplay(bool) {
@@ -53,8 +56,8 @@ let accordion = class Accordion {
     }
 
     test() {
-        for (let i = 0; i < 26; i++) {
-            this.addSlide('images/tim.png');
+        for (let i = 0; i < 5; i++) {
+            this.addSlide('images/te.jpeg');
         }
     }
 }
